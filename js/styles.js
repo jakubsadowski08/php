7ae7ef744496d71291e99d894933d19977e36c3e
jakubsadowski08 <33435745+jakubsadowski08@users.noreply.github.com>
2019-01-22 11:20:46 +0100

    Add files via upload

--- js/styles.js
+++ js/styles.js
@@ -1,17 +1,50 @@
-function swapStyleSheet(sheet){
+function swapStyleSheet(){
+    var coo = getCookie("css");
+    document.getElementById('pagestyle') .setAttribute('href', coo);
+    setCookie("css",coo);
+}
+function swapStyleSheets(sheet){
     document.getElementById('pagestyle').setAttribute('href', sheet);
+    setCookie("css",sheet);
 }
 function find()
 {
+
     for( i = 0; i < document.styleSheets.length; i++ )
     {
         var btn = document.createElement("BUTTON");
         var att = document.createAttribute("onclick");
         btn.appendChild(document.createTextNode(document.styleSheets[i].title));
-        att.value = "swapStyleSheet('" + document.styleSheets[i].href +"')";
+        if(i ==0)
+        {
+            att.value = "swapStyleSheets('" + "cs.css" +"')";
+        }
+        else
+        {
+            att.value = "swapStyleSheets('" + document.styleSheets[i].href +"')";
+        }
+
         btn.setAttributeNode(att);
         document.body.appendChild(btn);
     }
 }
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";";
+}
 
-swapStyleSheet(document.cookie);
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+swapStyleSheet()
+
